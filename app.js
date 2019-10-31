3ee7f49cd117fdf41428cc8125b20cd02db0fcb9
Teresa Cho <tcho@cca.edu>
2019-10-31 15:03:56 -0700

    installs morgan as middleware and declares it as const in app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+//Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
