7aee5a60ac11ab845085cf2f7fde447c3c0bd4fa
Teresa Cho <tcho@cca.edu>
2019-10-31 01:46:04 -0700

    creates a get request in orders.js with response of json message

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res, next) => {
+  res.status(200).json({
+    message: 'Orders were fetched'
+  });
+});
+
 
 
 module.exports = router;
