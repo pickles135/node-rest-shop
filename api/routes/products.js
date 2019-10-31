6c05f505c4f50bbe241e3b34407fc9061348c7e4
Teresa Cho <tcho@cca.edu>
2019-10-31 01:23:56 -0700

    adds a get and post request that will return a response of JSON message;

--- api/routes/products.js
+++ api/routes/products.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-  res,status(200).json({
-    message: "Handling GET request to /products"
+  res.status(200).json({
+    message: 'Handling GET request to /products'
   });
 });
 
