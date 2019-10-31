ff0cd53c95b8e156c80f013e82f5e69d9db2e37c
Teresa Cho <tcho@cca.edu>
2019-10-31 01:10:07 -0700

    adds both get and post requests with response of JSON message

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/')
+router.get('/', (req, res, next) => {
+  res,status(200).json({
+    message: "Handling GET request to /products"
+  });
+});
+
+router.post('/', (req, res, next) => {
+  res.status(200).json({
+    message: "Handling POST request to /products"
+  });
+});
+
+module.exports = router;
