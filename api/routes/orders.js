6b647a4304f5c60568581607c9f936780b1ffab5
Teresa Cho <tcho@cca.edu>
2019-10-31 01:53:51 -0700

    creates a get and delete request of an Order ID with response of json message

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -13,6 +13,19 @@ router.postt('/', (req, res, next) => {
   });
 });
 
+router.get('/:orderId', (req, res, next) => {
+  res.status(200).json({
+    message: 'Order details',
+    orderId = req,params,orderId
+  });
+});
+
+router.delete('/:orderId', (req, res, next) => {
+  res.status(200).json({
+    message: 'Order deteted',
+    orderId = req,params,orderId
+  });
+});
 
 
 module.exports = router;
