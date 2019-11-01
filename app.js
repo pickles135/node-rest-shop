80092112b6b1efb4e64c10c723d8b084bf014493
Teresa Cho <tcho@cca.edu>
2019-10-31 20:47:16 -0700

    installs morgan dependency that handles middleware logging of every request

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const morgan = require('morgan');
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+
 //Routes which should handle requests
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
