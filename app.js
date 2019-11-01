e402711e33b05a44832aa5e36edbb34d0533dab9
Teresa Cho <tcho@cca.edu>
2019-10-31 20:55:21 -0700

    handles error for any routes outside of ones built - product and orders, will send 404 code

--- app.js
+++ app.js
@@ -11,4 +11,10 @@ app.use(morgan('dev'));
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+app.use((req, res, next) => {
+  const error = new Error('Not found');
+  error.status(404);
+  next(error);
+})
+
 module.exports = app;
