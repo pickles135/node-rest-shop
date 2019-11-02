1d6598b45936257ae79ef553ba7d25e49601f908
Teresa Cho <tcho@cca.edu>
2019-11-02 14:15:12 -0700

    adds nodemon.json file with pw for dynamic routing and allowing mongoose to connect to mongo

--- app.js
+++ app.js
@@ -7,7 +7,14 @@ const mongoose = require('mongoose');
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
-mongoose.connect('mongodb+srv://node-shop:' + process.env.MONGO_ATLAS_PW + '@node-rest-shop-tfovr.mongodb.net/test?retryWrites=true&w=majority');
+mongoose.connect(
+  'mongodb+srv://node-shop:' +
+   process.env.MONGO_ATLAS_PW + 
+   '@node-rest-shop-tfovr.mongodb.net/test?retryWrites=true&w=majority',
+  {
+    useMongoClient: true
+  }
+);
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({extended: false}));
