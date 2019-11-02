cf08c658ec6da30a71e92f344ae764caf46f3cbe
Teresa Cho <tcho@cca.edu>
2019-11-02 16:18:46 -0700

    creates a models folder with product.js mongoose connect built and connected over to product.js in routes

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const Product = require('../models/product');
+
 router.get('/', (req, res, next) => {
   res.status(200).json({
     message: 'Handling GET request to /products'
