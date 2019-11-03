f5433a818f073c2562430b94cce8d8255b44dcab
Teresa Cho <tcho@cca.edu>
2019-11-02 21:22:12 -0700

    fixes a bug in prev commit, forgot to add .json for docs

--- api/routes/products.js
+++ api/routes/products.js
@@ -9,7 +9,7 @@ router.get('/', (req, res, next) => {
   .exec()
   .then(docs => {
     console.log(docs);
-    res.status(200)(docs);
+    res.status(200).json(docs);
   })
   .catch(err => {
     console.log(err);
