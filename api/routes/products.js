1629f61e1ff0deb6253d17bfbd83e4e72cec54a1
Teresa Cho <tcho@cca.edu>
2019-11-02 21:15:08 -0700

    creates error handling for if product id does not exist or if there is no information for item

--- api/routes/products.js
+++ api/routes/products.js
@@ -37,6 +37,11 @@ router.get('/:productID', (req, res, next) => {
   .exec()
   .then(doc => {
     console.log('From database ', doc);
+    if (doc) {
+      res.status(200).json(doc);
+    } else {
+      res.status(404).json({message: "No valid entry found for provided ID"});
+    }
     res.status(200).json(doc);
   })
   .catch(err => {
