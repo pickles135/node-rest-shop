f5d916b511915a29d3d26f13612196bc1f86e7ca
Teresa Cho <tcho@cca.edu>
2019-11-03 21:00:37 -0800

    imports mongoose to order.js file in routes and also requires the schema built in routes as well

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Order = require('../models/order');
 
 //Hande incoming GET requests tor /orders
 router.get('/', (req, res, next) => {
