123f2d983f395c63e027ae9a06bc14e6c7462d1a
Teresa Cho <tcho@cca.edu>
2019-11-03 18:12:37 -0800

    changes the res status for the get request for all with updated response

--- api/routes/products.js
+++ api/routes/products.js
@@ -14,7 +14,7 @@ router.get('/', (req, res, next) => {
       products: docs
     };
     // if (docs.length >=0) {
-      res.status(200).json(docs);
+      res.status(200).json(response);
     // } else {
     //   res.status(404).json({
     //     message: 'No entires found'
