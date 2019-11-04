439baa24f4b5b5c92b09bf65e0ca41a9719c6a74
Teresa Cho <tcho@cca.edu>
2019-11-04 13:09:20 -0800

    fixes typos and squashes bug for post result

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -13,24 +13,23 @@ router.get('/', (req, res, next) => {
 
 router.post('/', (req, res, next) => {
   const order = new Order({
-    _id: mongoose.Types.ObjectId(),
-    quanitity: req.body.quanitity,
-    product: req.body.productId
+      _id: mongoose.Types.ObjectId(),
+      quantity: req.body.quantity,
+      product: req.body.productId
   });
-  order.save()
-    .exec()
+  order
+    .save()
     .then(result => {
-        console.log(result);
-        res.status(201).json(result);
+      console.log(result);
+      res.status(201).json(result);
     })
     .catch(err => {
-        console.log(err);
-        res.status(500).json({
-          error: err
-        })
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
     });
 });
-
 router.get('/:orderId', (req, res, next) => {
   res.status(200).json({
     message: 'Order details',
