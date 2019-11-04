969166b4af1c3ecceeb594133c25dcc5c8b130ce
Teresa Cho <tcho@cca.edu>
2019-11-03 20:59:18 -0800

    completes order schema with product and quanitity defined

--- api/models/order.js
+++ api/models/order.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const orderSchema = mongoose.Schema({
   _id: mongoose.Schema.Types.ObjectId,
-  name: { type: String, required: true },
-  price: { type: Number, required: true}
+  product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
+  quantity: { type: Number, default: 1 }
 });
 
-module.exports = mongoose.model('Product, productSchema');
+module.exports = mongoose.model('Order, orderSchema');
