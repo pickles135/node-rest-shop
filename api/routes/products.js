192c515ec4f6a3480d29ba6f6cfc851da2f96c39
Teresa Cho <tcho@cca.edu>
2019-11-04 21:24:22 -0800

    adds multer package to our list of dependencies and imports/requires it in product.js file

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const multer = require('multer');
+const upload = multer({dest: '/uploads'});
 
 const Product = require("../models/product");
 
