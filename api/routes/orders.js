2a013e96bd0371639b05a76e9cf85ea79b1f650c
Teresa Cho <tcho@cca.edu>
2019-11-04 16:57:52 -0800

    creates an if statement to check if product is there and if not, return a json 404 message

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const mongoose = require('mongoose');
 
 const Order = require('../models/order');
+const Product = require('../models/product');
 
 //Hande incoming GET requests tor /orders
 router.get('/', (req, res, next) => {
@@ -33,28 +34,35 @@ router.get('/', (req, res, next) => {
 });
 
 router.post('/', (req, res, next) => {
-  const order = new Order({
-      _id: mongoose.Types.ObjectId(),
-      quantity: req.body.quantity,
-      product: req.body.productId
-  });
-  order
-    .save()
-    .then(result => {
-      console.log(result);
-      res.status(201).json({
-          message: 'Order stored',
-          createdOrder: {
-              _id: result._id,
-              product: result.product,
-              quantity: result.quantity
-          },
-          request:  {
-              type: 'GET',
-              url: 'http://localhost:3000/orders' + result._id
-          }
-      });
-    })
+    Product.findById(req.body.productId)
+      .then(product => {
+        if(!product) {
+            return res.status(404).json({
+                message: 'Product not found'
+            });
+        }
+          const order = new Order({
+            _id: mongoose.Types.ObjectId(),
+            quantity: req.body.quantity,
+            product: req.body.productId
+          });
+          return order.save();
+      })
+      .then(result => {
+        console.log(result);
+        res.status(201).json({
+            message: 'Order stored',
+            createdOrder: {
+                _id: result._id,
+                product: result.product,
+                quantity: result.quantity
+            },
+            request:  {
+                type: 'GET',
+                url: 'http://localhost:3000/orders' + result._id
+            }
+        });
+      })
     .catch(err => {
       console.log(err);
       res.status(500).json({
@@ -62,6 +70,7 @@ router.post('/', (req, res, next) => {
       });
     });
 });
+
 router.get('/:orderId', (req, res, next) => {
   res.status(200).json({
     message: 'Order details',
