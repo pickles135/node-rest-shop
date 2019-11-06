ebf53a82bed9a69dd1c3567faf4f92eec42ce861
Teresa Cho <tcho@cca.edu>
2019-11-05 23:37:08 -0800

    creates a file filter that will limit what kinds of filmes that will be allowed

--- api/routes/products.js
+++ api/routes/products.js
@@ -12,6 +12,15 @@ const storage = multer.diskStorage({
     } 
 });
 
+const fileFilter = (req, file, cb) => {
+    //reject a file
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+      cb(null, true);
+    } else {
+       cb(null, false);
+    }
+};
+
 const upload = multer({storage: storage, limits: {
     fileSize: 1024 * 1024 * 5
 }});
