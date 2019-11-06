935179388e7919e20c7b48be3069a749e482af20
Teresa Cho <tcho@cca.edu>
2019-11-05 23:07:49 -0800

    fixes a bug, defined the request, and response-needed to address file

--- api/routes/products.js
+++ api/routes/products.js
@@ -4,10 +4,10 @@ const mongoose = require("mongoose");
 const multer = require('multer');
 
 const storage = multer.diskStorage({
-    destination: function(req, res, cb) {
+    destination: function(req, file, cb) {
       cb(null, './uploads/');
     },
-    filename: function(req, res, cb) {
+    filename: function(req, file, cb) {
       cb(null, new Date().toISOString() + file.originalname);
     } 
 });
