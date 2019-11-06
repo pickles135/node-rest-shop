d5b1795592f12ec7ef32e9d2ff63207cd6bc423e
Teresa Cho <tcho@cca.edu>
2019-11-05 23:28:01 -0800

    adds a limit size to the image in the upload const that was built

--- api/routes/products.js
+++ api/routes/products.js
@@ -12,7 +12,9 @@ const storage = multer.diskStorage({
     } 
 });
 
-const upload = multer({storage: storage});
+const upload = multer({storage: storage, limits: {
+    fileSize: 1024 * 1024 * 5
+}});
 
 const Product = require("../models/product");
 
