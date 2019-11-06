8ff0cc1f24a2261a4f31cdb0cef832177ac4970e
Teresa Cho <tcho@cca.edu>
2019-11-06 00:53:13 -0800

    adds middleware component that will allow uploads onto the url and space

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ mongoose.connect(
 );
 
 app.use(morgan('dev'));
-app.use(express.static('uploads'));
+app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
