9d52e94a0b93e2fcc6b7fd33a0231c3b872829d8
Teresa Cho <tcho@cca.edu>
2019-11-06 18:32:24 -0800

    adds new user to POST route for creation of new user however, still need to encrypt password

--- api/routes/user.js
+++ api/routes/user.js
@@ -5,7 +5,11 @@ const mongoose = require('mongoose');
 const User = require('../models/user');
 
 router.post('/signup', (req, res, next) => {
-  
+  const user = new User({
+      _id: new mongoose.Types.ObjectId(),
+      email: req.body.email,
+      password: req.body.password
+  });
 });
 
 module.exports = router;
