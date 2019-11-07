f317f4bd51a2c6c091925ebcf5bc6cf8f663408c
Teresa Cho <tcho@cca.edu>
2019-11-06 23:58:39 -0800

    creates a JWT key and creates a token with experation

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -59,8 +60,18 @@ router.post('/login', (req, res, next) => {
               })
           } 
           if (result) {
+            const token = jwt.sign({
+                email: user[0].email,
+                userId: user[0]._id
+            }, 
+            process.env.JWT_KEY, 
+            {
+              expiresIn: '1h'
+            }
+            )
             return res.status(200).json({
-                message: 'Auth sucessful'
+                message: 'Auth sucessful',
+                token: token
             });
           }
           res.status(401).json({
