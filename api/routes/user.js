b465847ae3177f2757d57129c693e64aef377bc9
Teresa Cho <tcho@cca.edu>
2019-11-06 22:48:42 -0800

    creates a conditional to check if the email already exists

--- api/routes/user.js
+++ api/routes/user.js
@@ -6,31 +6,41 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 router.post("/signup", (req, res, next) => {
-  bcrypt.hash(req.body.email, 10, (err, hash) => {
-      if (err) {
-        error: err
-      } else {
-        const user = new User({
-            _id: new mongoose.Types.ObjectId(),
-            email: req.body.email,
-            password: hash
-        });
-        user
-          .save()
-          .then(result => {
-            console.log(result);
-            res.status(201).json({
-                message: 'User created'
+    User.find({ email: req.body.email })
+      .exec()
+      .then(user => {
+          if (user) {
+            return res.status(409).json({
+                message: 'Email exists'
             });
-          })
-          .catch(err => {
-            console.log(err);
-            res.status(500).json({
-              error: err
+          } else {
+            bcrypt.hash(req.body.password, 10, (err, hash) => {
+                if (err) {
+                  error: err
+                } else {
+                  const user = new User({
+                      _id: new mongoose.Types.ObjectId(),
+                      email: req.body.email,
+                      password: hash
+                  });
+                  user
+                    .save()
+                    .then(result => {
+                      console.log(result);
+                      res.status(201).json({
+                          message: 'User created'
+                      });
+                    })
+                    .catch(err => {
+                      console.log(err);
+                      res.status(500).json({
+                        error: err
+                      });
+                    })
+                }
             });
-          })
-      }
-  });
+          };
+      })
 });
 
 module.exports = router;
