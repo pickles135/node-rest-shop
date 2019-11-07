c9f6b862e9ffbd996ea985438169f039f4d42f22
Teresa Cho <tcho@cca.edu>
2019-11-06 18:25:15 -0800

    creates a POST for signup for user creation, also imports the user schema model

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,5 +2,10 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
+const User = require('../models/user');
+
+router.post('/signup', (req, res, next) => {
+  
+});
 
 module.exports = router;
