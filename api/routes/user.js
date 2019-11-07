78134639c1188c611dbc166385a2a0ffea6bf075
Teresa Cho <tcho@cca.edu>
2019-11-06 22:21:58 -0800

    installs bcrypt package and is defined in user route with a post req that will hash pw and email when new user is created

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const User = require('../models/user');
 
-router.post('/signup', (req, res, next) => {
-  const user = new User({
-      _id: new mongoose.Types.ObjectId(),
-      email: req.body.email,
-      password: req.body.password
+router.post("/signup", (req, res, next) => {
+  bcrypt.hash(req.body.email, 10, (err, hash) => {
+      if (err) {
+        error: err
+      } else {
+        const user = new User({
+            _id: new mongoose.Types.ObjectId(),
+            email: req.body.email,
+            password: hash
+        });
+      }
   });
 });
 
