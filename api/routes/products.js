9da7c643a61b40e4c1cd588be9454955b2bb2858
Teresa Cho <tcho@cca.edu>
2019-11-11 18:02:51 -0800

    fixes typos and indentations

--- api/routes/products.js
+++ api/routes/products.js
@@ -5,29 +5,29 @@ const multer = require('multer');
 const checkAuth = require('../middleware/check-auth');
 
 const storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-      cb(null, './uploads/');
-    },
-    filename: function(req, file, cb) {
-      cb(null, new Date().toISOString() + file.originalname);
-    } 
+  destination: function(req, file, cb) {
+    cb(null, './uploads/');
+  },
+  filename: function(req, file, cb) {
+    cb(null, new Date().toISOString() + file.originalname);
+  }
 });
 
 const fileFilter = (req, file, cb) => {
-    //reject a file
-    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
-      cb(null, true);
-    } else {
-       cb(null, false);
-    }
+  // reject a file
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
 };
 
 const upload = multer({
-    storage: storage, 
-    limits: {
-      fileSize: 1024 * 1024 * 5
-    },
-    fileFilter: fileFilter
+  storage: storage,
+  limits: {
+    fileSize: 1024 * 1024 * 5
+  },
+  fileFilter: fileFilter
 });
 
 const Product = require("../models/product");
@@ -73,7 +73,7 @@ router.post("/", checkAuth, upload.single('productImage'), (req, res, next) => {
     _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
     price: req.body.price,
-    productImage: req.file.path
+    productImage: req.file.path 
   });
   product
     .save()
