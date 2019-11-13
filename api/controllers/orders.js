bcae7321d77add1e3d2f23fb721403f6ee7c9c0f
Teresa Cho <tcho@cca.edu>
2019-11-13 01:04:37 -0800

    adds a get order route into controllers

--- api/controllers/orders.js
+++ api/controllers/orders.js
@@ -31,7 +31,7 @@ exports.orders_get_all = (req, res, next) => {
     });
   };
 
-  exports.orders_create_orders = (req, res, next) => {
+exports.orders_create_orders = (req, res, next) => {
     Product.findById(req.body.productId)
       .then(product => {
         if (!product) {
@@ -68,3 +68,28 @@ exports.orders_get_all = (req, res, next) => {
         });
       });
   };
+
+exports.orders_get_order = (req, res, next) => {
+  Order.findById(req.params.orderId)
+    .populate('product')
+    .exec()
+    .then(order => {
+      if (!order) {
+        return res.status(404).json({
+          message: "Order not found"
+        });
+      }
+      res.status(200).json({
+        order: order,
+        request: {
+          type: "GET",
+          url: "http://localhost:3000/orders"
+        }
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
+};
--- api/routes/orders.js
+++ api/routes/orders.js
@@ -6,37 +6,14 @@ const checkAuth = require('../middleware/check-auth');
 const Order = require("../models/order");
 const Product = require("../models/product");
 
-const OrdersControllers = require('../controllers/orders');
+const OrdersController = require('../controllers/orders');
 
 // Handle incoming GET requests to /orders
-router.get("/", checkAuth, OrdersControllers.orders_get_all);
+router.get("/", checkAuth, OrdersController.orders_get_all);
 
-router.post("/", checkAuth, OrdersControllers.orders_create_orders);
+router.post("/", checkAuth, OrdersController.orders_create_orders);
 
-router.get("/:orderId", checkAuth, (req, res, next) => {
-  Order.findById(req.params.orderId)
-    .populate('product')
-    .exec()
-    .then(order => {
-      if (!order) {
-        return res.status(404).json({
-          message: "Order not found"
-        });
-      }
-      res.status(200).json({
-        order: order,
-        request: {
-          type: "GET",
-          url: "http://localhost:3000/orders"
-        }
-      });
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: err
-      });
-    });
-});
+router.get("/:orderId", checkAuth, OrdersController.orders_get_order);
 
 router.delete("/:orderId", checkAuth, (req, res, next) => {
   Order.remove({ _id: req.params.orderId })
