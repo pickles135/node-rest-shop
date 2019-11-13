4fee2da0e2b8b3f9c782215ff7e19943e0ae45b1
Teresa Cho <tcho@cca.edu>
2019-11-13 01:33:25 -0800

    creates post controller for products

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -37,3 +37,36 @@ exports.products_get_all = (req, res, next) => {
       });
     });
 };
+
+exports.products_create_product = (req, res, next) => {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
+    name: req.body.name,
+    price: req.body.price,
+    productImage: req.file.path 
+  });
+  product
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: "Created product successfully",
+        createdProduct: {
+            name: result.name,
+            price: result.price,
+            _id: result._id,
+            request: {
+                type: 'GET',
+                url: "http://localhost:3000/products/" + result._id
+            }
+        }
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({
+        error: err
+      });
+    });
+};
+
--- api/routes/products.js
+++ api/routes/products.js
@@ -36,37 +36,7 @@ const Product = require("../models/product");
 
 router.get("/", ProductController.products_get_all);
 
-router.post("/", checkAuth, upload.single('productImage'), (req, res, next) => {
-  const product = new Product({
-    _id: new mongoose.Types.ObjectId(),
-    name: req.body.name,
-    price: req.body.price,
-    productImage: req.file.path 
-  });
-  product
-    .save()
-    .then(result => {
-      console.log(result);
-      res.status(201).json({
-        message: "Created product successfully",
-        createdProduct: {
-            name: result.name,
-            price: result.price,
-            _id: result._id,
-            request: {
-                type: 'GET',
-                url: "http://localhost:3000/products/" + result._id
-            }
-        }
-      });
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json({
-        error: err
-      });
-    });
-});
+router.post("/", checkAuth, upload.single('productImage'), ProductController.products_create_product);
 
 router.get("/:productId", (req, res, next) => {
   const id = req.params.productId;
