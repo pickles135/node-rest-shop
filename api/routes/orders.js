43a3f1c64cea52da90de7530668cd219bf3e8752
Teresa Cho <tcho@cca.edu>
2019-11-12 23:05:49 -0800

    adds a controller to handle the get-all route in orders.js

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -6,34 +6,10 @@ const checkAuth = require('../middleware/check-auth');
 const Order = require("../models/order");
 const Product = require("../models/product");
 
+import OrdersControllers from '../controllers/orders';
+
 // Handle incoming GET requests to /orders
-router.get("/", checkAuth, (req, res, next) => {
-  Order.find()
-    .select("product quantity _id")
-    .populate('product', 'name')
-    .exec()
-    .then(docs => {
-      res.status(200).json({
-        count: docs.length,
-        orders: docs.map(doc => {
-          return {
-            _id: doc._id,
-            product: doc.product,
-            quantity: doc.quantity,
-            request: {
-              type: "GET",
-              url: "http://localhost:3000/orders/" + doc._id
-            }
-          };
-        })
-      });
-    })
-    .catch(err => {
-      res.status(500).json({
-        error: err
-      });
-    });
-});
+router.get("/", OrdersControllers.orders_get_all);
 
 router.post("/", checkAuth, (req, res, next) => {
   Product.findById(req.body.productId)
