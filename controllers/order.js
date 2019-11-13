3c9c1b19eeee71fa80d4e5fc899fb6eca67fbc62
Teresa Cho <tcho@cca.edu>
2019-11-12 23:01:08 -0800

    creates controllers folder and grabs the get all route in orders

---
+++ controllers/order.js
@@ -0,0 +1,29 @@
+const Order = require('../models/order');
+
+exports.orders_get_all = checkAuth, (req, res, next) => {
+  Order.find()
+    .select("product quantity _id")
+    .populate('product', 'name')
+    .exec()
+    .then(docs => {
+      res.status(200).json({
+        count: docs.length,
+        orders: docs.map(doc => {
+          return {
+            _id: doc._id,
+            product: doc.product,
+            quantity: doc.quantity,
+            request: {
+              type: "GET",
+              url: "http://localhost:3000/orders/" + doc._id
+            }
+          };
+        })
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
+  };
